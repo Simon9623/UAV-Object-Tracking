03169e0747b3f1af6925eb2e0775be0dcf616f6b
Simmon <ston96237@gmail.com>
2025-06-27 22:55:50 +0800

    version 2.0

--- Module/control_system.py
+++ Module/control_system.py
@@ -106,7 +106,7 @@ class AutonomousController:
         logger.debug(f"Control Errors: x={error_x:.1f}, y={error_y:.1f}, dist_err={(self.pid_distance.setpoint - distance_to_target):.2f}")
         logger.debug(f"Control Outputs: vx={vx_cmd:.2f}, vz={vz_cmd:.2f}, yaw={yaw_rate_cmd:.2f}")
 
-        return {'vx': vx_cmd, 'vy': 0, 'vz': vz_cmd, 'yaw_rate': yaw_rate_cmd}
+        return {'vx': vx_cmd, 'vy': 0, 'vz': 0, 'yaw_rate': yaw_rate_cmd}
     
     def reset(self):
         """重置所有 PID 控制器。"""
